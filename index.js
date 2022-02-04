9cd4ecbfd43a696ed3c8eacdd25dea76c6877108
aborrelli20 <71987074+aborrelli20@users.noreply.github.com>
2022-02-04 10:39:42 -0500

    added functionality to show equation on screen

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-var randomNumberGenerator = function (size) {
-  return Math.ceil(Math.random() * size);
-}
+$(document).ready(function () {
+  var currentQuestion;
+  var randomNumberGenerator = function (size) {
+    return Math.ceil(Math.random() * size);
+  }
 
-var questionGenerator = function () {
-  var question = {};
-  var num1 = randomNumberGenerator(10);
-  var num2 = randomNumberGenerator(10);
+  var questionGenerator = function () {
+    var question = {};
+    var num1 = randomNumberGenerator(10);
+    var num2 = randomNumberGenerator(10);
 
-  question.answer = num1 + num2;
-  question.equation = String(num1) + " + " + String(num2);
+    question.answer = num1 + num2;
+    question.equation = String(num1) + " + " + String(num2);
 
-  return question;
-}
-console.log(questionGenerator());
-console.log(questionGenerator());
+    return question;
+  }
+
+  currentQuestion = questionGenerator();
+  $('#equation').text(currentQuestion.equation);
+});
